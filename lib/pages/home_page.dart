4dfa7d3188bbce6366c662a0d795a1ca6552c8c7
ErickAldair <aldair-gym97@gmail.com>
2021-03-16 17:59:06 -0500

    version 1

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/provider/ui_provider.dart';
+import 'package:qr_reader/widgets/custom_navigatorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
+class HomePage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete_forever),
+             onPressed: (){}
+          )
+        ],
+      ),
+      body: _HomePageBody(),
+
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+
+class _HomePageBody extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    final uiProvider = Provider.of<UiProvider>(context);
+   final currentIndex = uiProvider.selectedMenuOpt;
+
+   switch(currentIndex){
+
+     case 0:
+     return MapasPage();
+
+     case 1:
+     return DireccionesPage();
+
+     default:
+     return MapasPage();
+   }
+  }
+}
---
+++ lib/widgets/custom_navigatorbar.dart
@@ -0,0 +1,38 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/provider/ui_provider.dart';
+
+
+
+class CustomNavigationBar extends StatelessWidget {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currenIndex = uiProvider.selectedMenuOpt;
+
+
+    return BottomNavigationBar(
+      onTap: (int i ) => uiProvider.selectedMenuOpt=i,
+      currentIndex: currenIndex,
+      elevation: 0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          label: 'Mapa'
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.compass_calibration),
+          label: 'Direcciones'
+        )
+      ],
+
+    );
+  }
+}
